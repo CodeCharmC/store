a8d73cc1820eb7e941b01ac7f20ea1b53fe9d85e
CodeCharmC <moni.akhtar1066@gmail.com>
2024-10-20 23:29:17 +0600

    1st: connect to the database and run the server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('04 Store API')
+//1st: connect to the database
+require('dotenv').config(); 
+const connectDB = require('./backend/db/connect');
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+   try {
+      //1th: connect to the database
+      await connectDB(process.env.MONGO_URI);
+      app.listen(port, () => {
+         console.log(`Server is listening on port ${port}...`);
+      });
+   } catch (error) {
+      console.log(error);
+   }
+};
+
+start();
