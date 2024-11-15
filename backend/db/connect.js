293adb218d2b3385e1e0e0e1980069d845906382
CodeCharmC <moni.akhtar1066@gmail.com>
2024-11-15 22:17:45 +0600

    modify connect js

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose')
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
+const connectDB = async(url) => {
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1); // Exit the process with failure
+  }
 }
 
 module.exports = connectDB
+
+
+/**MONGO_URL=mongodb+srv://moni:moni@store-manager.asthq.mongodb.net/?retryWrites=true&w=majority&appName=store-manager */
